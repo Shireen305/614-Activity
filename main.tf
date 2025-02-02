144aba03396c54c80447cd7ab85316efa9132798
Shireen305 <137711113+Shireen305@users.noreply.github.com>
2025-02-02 15:17:30 -0500

    Terraform Wordpress file uploaded

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+locals {
+    aws_key = "CA_AWS_KEY"   # Change this to your desired AWS region
+  }
+
+resource "aws_instance" "my_server" {
+   ami           = data.aws_ami.amazonlinux.id
+   instance_type = var.instance_type
+   key_name      = "${local.aws_key}"                  
+   user_data = "${file(wp_install.sh)}"
+   security_groups = [aws_security_group.ec2_sg]
+   tags = {
+     Name = "my ec2"
+   }                  
+ }
---
+++ securitygroup.tf
@@ -0,0 +1,27 @@
+# Security Groups
+resource "aws_security_group" "ec2_sg" {
+  name        = "wordpress_ec2_sg"
+  description = "Security group for WordPress EC2 instance"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
