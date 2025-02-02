7565687b77a6603fc855ec36d8c9f4e9e79e65d1
Shireen305 <137711113+Shireen305@users.noreply.github.com>
2025-02-02 16:05:02 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
       backend "s3" {
-      bucket = "my-terraform-state-bucket"
+      bucket = "my-terraform-state-bucket-sm"
       key    = "prod/terraform.tfstate"
       region = "us-east-1"
 }
